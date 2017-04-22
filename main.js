1c38626c4b1e1ad44ebffbddc4f9b42850ce45fc
Codayblue <codayblue@gmail.com>
2017-04-22 11:44:57 -0700

    Tried to add sub support to find the API has changed and now a client id is required. I will work on this at a later date.

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
+// Get Node modules
 var fs = require('fs');
 var https = require('https');
+
+// Get prompt module
 var prompt = require('prompt');
+
+// Set some module variables
 var close = true;
 var user = [];
 var num = "";
 
+// Reads the settings file
+// then parses the results and returns a json object
 function getSettings() {
 	var settings = [];
 	var fdata = fs.readFileSync('settings.json', 'utf8');
@@ -15,41 +22,43 @@ function getSettings() {
 	return settings;
 }
 
+// Sends the request to the twitch api
+// then when we get data back (ASYNC) we store it in
+// our module variable to be printed to the file later.
 function getData(username) {
 
-	var idata = {};
+    var idata = {};
 
-	var options = {
-  		hostname: 'api.twitch.tv',
-  		port: 443,
- 		path: '/kraken/channels/'+ username +'/follows',
- 		method: 'GET',
- 		headers: {
-    		'Content-Type': 'Accept: application/vnd.twitchtv.v3+json',
-  		}
-	};
+    var options = {
+        hostname: 'api.twitch.tv',
+        port: 443,
+        path: '/kraken/channels/'+ username +'/follows',
+        method: 'GET',
+        headers: {
+        'Content-Type': 'Accept: application/vnd.twitchtv.v5+json',
+        }
+    };
 
-	var req = https.request(options, function(res){
-		res.setEncoding('utf8');
-	  	res.on('data', function (chunk) {
-	  		var obj = JSON.parse(chunk);
-	    	idata = obj._total;
-	    	num = String(idata);
-	    	
-	    	//console.log(idata);
-  		});
+    var req = https.request(options, function(res){
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            var obj = JSON.parse(chunk);
 
-	});
+            idata = obj._total;
+            num = String(idata);
+            console.log(obj);
+        });
 
-	req.on('error', function(err){
-		console.error(err);
-	});
+    });
 
-	req.end();
+    req.on('error', function(err){
+        console.error(err);
+    });
 
-	//return req;
+    req.end();
 }
 
+// Gets the data ready to be written to the file
 function setData(data) {
 
 	var set = [];
@@ -57,62 +66,66 @@ function setData(data) {
 
 	var sent = set.text + " Goal " + data + "/" + set.victory;
 
-	//console.log(data);
-
 	writeToFile('goal.txt', sent);
 }
 
+// Writes data to a file.
 function writeToFile(fileName, data) {
 
 	fs.writeFileSync(fileName, data);
 
 }
 
-function setSettings(user, time, goal, intval) {
+// Get the settings ready to be written to the settings file.
+function setSettings(user, time, goal, intval, subs) {
 	var settings = {};
 	var settingsFile = '';
-	
+
 	settings = {
 					username: user,
 					text: time,
 					victory: goal,
-					refresh: intval			
+					refresh: intval,
+                    subs: subs
 			   };
-    settingsFile = '{"username":"' + settings.username + 
-				   '","text":"' + settings.text + 
-				   '","victory":"' + settings.victory + 
+    settingsFile = '{"username":"' + settings.username +
+				   '","text":"' + settings.text +
+				   '","victory":"' + settings.victory +
 				   '","refresh":"' + settings.refresh +
+                    '","subs":"' + settings.subs +
 				   '"}';
-			   
+
     writeToFile('settings.json', settingsFile);
-	
+
 }
 
+// Start the prompt
 prompt.start();
 
+// Create the menu
 console.log('Please Select a function');
 console.log('1. run');
 console.log('2. set settings');
 
+// Get the option the user would like to do
 prompt.get(['run'], function(err, result){
-	
-if(result.run == 1){
-	user = getSettings();
-	var intval = user.refresh * 1000;
-	getData(user.username);
-	
-	console.log("To exit press ctrl-c");
-	setInterval(function() {
-		getData(user.username);
-		console.log("it fired");
-		setData(num);
-	}, intval);
-} else if (result.run == 2) {
-	prompt.get(['username','timeframe','goal','interval'], function(err, result){
-		setSettings(result.username, result.timeframe, result.goal, result.interval);
-	});
-} else {
-	console.log('Error: Please select a function as listed.');
-}
+
+    if(result.run == 1){
+        user = getSettings();
+        var intval = user.refresh * 1000;
+        getData(user.username);
+
+        console.log("To exit press ctrl-c");
+        setInterval(function() {
+            getData(user.username);
+            console.log("it fired");
+        }, intval);
+    } else if (result.run == 2) {
+        prompt.get(['username','timeframe','goal','interval',"subs"], function(err, result){
+            setSettings(result.username, result.timeframe, result.goal, result.interval, result.subs);
+        });
+    } else {
+        console.log('Error: Please select a function as listed.');
+    }
 
 });
